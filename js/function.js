12b235a35febe95b69c88a8eb64fb3ae5ac8333f
Jorge Wendell <jorge@swapi.com.br>
2013-05-03 07:45:34 -0300

    Was decoupled from HTML CSS and JS

---
+++ js/function.js
@@ -0,0 +1,13 @@
+/*
+ * Função que seta o tipo de animação de transição
+ */
+
+$(function() {
+  var $list = $('ul li');
+    $list.filter(':first').addClass('animated flipInX');
+    setInterval(function() {
+        if( $list.filter('.flipInX').index() !== $list.length) {
+            $list.filter('.flipInX').removeClass('animated flipInX').next().addClass('animated flipInX');
+        }
+    }, 400);
+});
